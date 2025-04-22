04a6dc43fb351db5d0715ad0f8eb95d081bf88e0
Rajaguru A <162814363+Rajaguru1701@users.noreply.github.com>
2025-04-22 11:45:26 +0530

    Add files via upload

---
+++ PQ5.java
@@ -0,0 +1,22 @@
+public class PQ5 {
+    public static void main(String[] args) {
+        int n = 5;
+        int val = 1,inc = 5;
+        for (int i = 1; i <= n ; i++) {
+            int space = n-i;
+            int temp = val;
+            for (int s = 1; s <= space; s++) {
+                System.out.print("   ");
+            }
+            int k = n-i+1;
+            for (int j = 1; j <=i ; j++) {
+                System.out.print(temp+" ");
+                temp-=k;
+                k++;
+            }
+            System.out.println();
+            val+=inc;
+            inc--;
+        }
+    }
+}
